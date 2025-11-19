5cf1b86f4828bc4c0327179f6757704a8a6450d1
pgm-arnegoossens <arnegoos@student.arteveldehs.be>
2025-11-19 16:10:09 +0100

    Oefeningen tem 9.1

--- week_1/les_1/oefeningen/exercises.js
+++ week_1/les_1/oefeningen/exercises.js
@@ -81,3 +81,20 @@ function showTasks(tasks){
 }
 
 showTasks(todoList);
+
+//Oefening 9.1
+function checkAge(age) {
+    if (age > 18) {
+        return "You good.";
+    } else {
+        return "Did parents allow you?";
+    }
+}
+console.log(checkAge(20));
+console.log(checkAge(10));
+
+function checkAge2(age){
+    return age > 18 ? "You good." : "Did parents allow you?";
+}
+console.log(checkAge2(20));
+console.log(checkAge2(10));
